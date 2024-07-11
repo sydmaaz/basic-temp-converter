b31b50795667b7c9113baa2addd3dbe05ba3f8ba
sydmaaz <158038923+sydmaaz@users.noreply.github.com>
2024-07-11 23:39:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const usript = document.getElementById("usript")
+const toFahrenheit = document.getElementById("toFahrenheit")
+const toCelsius = document.getElementById("toCelsius")
+const result = document.getElementById("result")
+let temp;
+
+function convert(){
+    if(toFahrenheit.checked){
+        temp = usript.value
+        temp = (temp * 9/5) + 32
+        result.textContent = temp.toFixed(1) + "°F"
+    }
+    else if(toCelsius.checked){
+        temp = usript.value
+        temp = (temp - 32) * 5/9
+        result.textContent = temp.toFixed(1) + "°C"
+    }
+    else{
+        result.textContent = "Please select a unit"
+    }
+}
